dcdd106d22afc9c514bd7aea8a6acc39a4b337af
Himanshu Yadav <himaanshuuyadav@gmail.com>
2026-04-06 15:00:32 +0530

    refactor-viewmodel: update SaveButtonState
    Apply update changes in app/src/main/java/com/prantiux/milktick/viewmodel/SaveButtonState.kt.

--- app/src/main/java/com/prantiux/milktick/viewmodel/SaveButtonState.kt
+++ app/src/main/java/com/prantiux/milktick/viewmodel/SaveButtonState.kt
@@ -2,8 +2,13 @@ package com.prantiux.milktick.viewmodel
 
 enum class SaveButtonState {
     SAVE,
+    UPDATE,
+    REMOVE,
     LOADING,
+    LOADING_UPDATE,
+    LOADING_REMOVE,
     SAVED,
+    UPDATED,
     REMOVED,
     HIDDEN
 }
